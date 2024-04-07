b2a06bd718e8a13a5036dbfc5c33ae78307f3541
GrimDesp <d.myroniukwork@gmail.com>
2024-04-07 16:17:31 +0300

    Before Scaffold

---
+++ BoardGameShop.DAL/Entities/Configuration/IOrderItems.cs
@@ -0,0 +1,11 @@
+﻿namespace BoardGameShop.DAL.Entities.Configuration
+{
+    internal class IOrderItems
+    {
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public int ItemCost { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
